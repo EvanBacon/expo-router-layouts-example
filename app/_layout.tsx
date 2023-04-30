017737debf53c549cd20fdb078dafe67d0b9ddb0
evanbacon <bacon@expo.io>
2023-04-30 17:46:14 -0500

    fix seo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Tabs } from "expo-router";
-
+import Head from "expo-router/head";
 export default function Layout() {
-  return <Tabs />;
+  return (
+    <>
+      <Head>
+        <title>Expo Router Layouts Demo</title>
+        <meta name="description" content="Expo Router Layouts Demo" />
+      </Head>
+      <Tabs
+        screenOptions={{
+          tabBarInactiveTintColor: "#000",
+        }}
+      />
+    </>
+  );
 }
