df428b4078f3382353ed79a4ff5226e48d514d83
evanbacon <bacon@expo.io>
2023-05-05 16:34:08 -0600

    add custom transitions

--- app/custom-stack/_layout.tsx
+++ app/custom-stack/_layout.tsx
@@ -1,17 +1,79 @@
+import { useState } from "react";
+import { Button, Text, View } from "react-native";
 import { CustomStack } from "../../layouts/custom-stack";
+import { cardStyleInterpolator } from "../../transitions/custom-transition";
 
 export const unstable_settings = {
   initialRouteName: "index",
 };
 
+const animationSettings = [
+  ["default", {}],
+  [
+    "custom slide",
+    {
+      cardStyleInterpolator,
+    },
+  ],
+  [
+    "vertical",
+    {
+      gestureDirection: "vertical",
+      cardStyleInterpolator: ({ current, layouts }) => {
+        return {
+          cardStyle: {
+            transform: [
+              {
+                translateY: current.progress.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [layouts.screen.height, 0],
+                }),
+              },
+            ],
+          },
+        };
+      },
+    },
+  ],
+] as const;
+
 export default function Layout() {
+  const [index, setIndex] = useState(0);
+  const selected = animationSettings[index];
   return (
-    <CustomStack
-      screenOptions={
-        {
+    <View style={{ flex: 1 }}>
+      <CustomStack
+        screenOptions={{
+          ...selected[1],
           // API Reference: https://reactnavigation.org/docs/stack-navigator#options
-        }
-      }
-    />
+        }}
+      />
+      <View
+        style={{
+          borderTopColor: "#eee",
+          borderTopWidth: 1,
+          backgroundColor: "white",
+        }}
+      >
+        <Text style={{ padding: 8, opacity: 0.6 }}>
+          Press these buttons to change the layout transition{"\n"}↓ then press
+          "Next" to see the transition. ↓
+        </Text>
+        <View
+          style={{
+            flexDirection: "row",
+          }}
+        >
+          {animationSettings.map((value, i) => (
+            <Button
+              color={index === i ? "#2196F3" : "#ccc"}
+              title={value[0]}
+              key={String(i)}
+              onPress={() => setIndex(i)}
+            />
+          ))}
+        </View>
+      </View>
+    </View>
   );
 }
---
+++ transitions/custom-transition.ts
@@ -0,0 +1,46 @@
+import { Animated } from "react-native";
+
+export const cardStyleInterpolator = ({
+  current,
+  next,
+  inverted,
+  layouts: { screen },
+}) => {
+  const progress = Animated.add(
+    current.progress.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 1],
+      extrapolate: "clamp",
+    }),
+    next
+      ? next.progress.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, 1],
+          extrapolate: "clamp",
+        })
+      : 0
+  );
+
+  return {
+    cardStyle: {
+      transform: [
+        {
+          translateX: Animated.multiply(
+            progress.interpolate({
+              inputRange: [0, 1, 2],
+              outputRange: [
+                screen.width, // Focused, but offscreen in the beginning
+                0, // Fully focused
+                //I changed this line only
+                //screen.width * -0.3 // Fully unfocused
+                -screen.width,
+              ],
+              extrapolate: "clamp",
+            }),
+            inverted
+          ),
+        },
+      ],
+    },
+  };
+};
